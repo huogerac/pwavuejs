b7fcedb48dc50bd88526215d99feacdce63010b8
Roger Camargo <huogerac@gmail.com>
2021-09-02 23:04:43 -0300

    Bump version v0.1.5

--- src/components/HelloPWA.vue
+++ src/components/HelloPWA.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      This is a PWA testing. v1.4
+      This is a PWA testing. v1.5
       <a
         href="https://github.com/huogerac/pwavuejs"
         target="_blank"
